4a8824456a0a0da4ab719be88647e55ad85a58d0
montsita <montsest.boi@gmail.com>
2024-08-26 13:07:06 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros;
+        public int diferencia(int a, int b) {
+            int resultado = a + b; // Lógica del método
+            return resultado; // Devolver el resultado
+        }
+
+
+    }
+}
